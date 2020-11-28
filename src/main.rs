e3d38879228f5ce7830ba69806b4b4b0d2121a99
Yuki Okushi <huyuumi.dev@gmail.com>
2020-11-29 06:56:32 +0900

    Use clap to make rpg a CLI tool

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::Clap;
+
+/// CLI tool for running your code on the Rust Playground.
+#[derive(Clap)]
+#[clap(version = "0.1.0", author = "Yuki Okushi <huyuumi.dev@gmail.com>")]
+struct Opts {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Clap)]
+enum SubCommand {
+    Run(Run),
+}
+
+/// A subcommand for running a snippet on the playground.
+#[derive(Clap)]
+struct Run {
+    /// File name to execute.
+    file_name: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+
+    match opts.subcmd {
+        SubCommand::Run(t) => {
+            println!("file name is {}.", t.file_name);
+        }
+    }
 }
