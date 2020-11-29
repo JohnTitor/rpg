d19aadb4a00f00090a14a20a451886db1b883326
Yuki Okushi <huyuumi.dev@gmail.com>
2020-11-30 00:50:50 +0900

    Tweak share implementation

--- src/share.rs
+++ src/share.rs
@@ -3,6 +3,9 @@ use std::io::prelude::*;
 use serde::Deserialize;
 use clap::Clap;
 
+/// Returns Gist URL, ID, and given code itself.
+const GIST_GEN_URL: &str = "https://play.rust-lang.org/meta/gist/";
+
 /// A subcommand for generating permanent playground URL.
 #[derive(Clap)]
 pub struct Share {
@@ -19,15 +22,17 @@ pub(crate) fn share(share: &Share) -> std::io::Result<String> {
     let mut file = File::open(&share.file_name)?;
     let mut code = String::new();
     file.read_to_string(&mut code)?;
-    let mut map = HashMap::new();
-    map.insert("code", &code);
+
+    let mut req_json = HashMap::new();
+    req_json.insert("code", &code);
 
     let client = reqwest::blocking::Client::new();
-    let res: GistRes = client.post("https://play.rust-lang.org/meta/gist/")
-        .json(&map)
+    let res: GistRes = client.post(GIST_GEN_URL)
+        .json(&req_json)
         .send()
         .unwrap()
         .json()
         .unwrap();
+
     Ok(res.id)
 }
