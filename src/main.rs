8bf478397123d1ac6044db80e5f1c934a33e49a2
Yuki Okushi <huyuumi.dev@gmail.com>
2020-11-29 16:53:41 +0900

    Add some options for `run` command

--- src/main.rs
+++ src/main.rs
@@ -23,44 +23,51 @@ enum SubCommand {
 struct Run {
     /// File name to execute.
     file_name: String,
+    #[clap(short, long, default_value = "stable")]
+    /// rustc version, panic if not `stable`, `beta`, or `nightly`.
+    version: String,
+    #[clap(short, long, default_value = "debug")]
+    /// Opt level, panic if not `debug` or `release`.
+    mode: String,
+    #[clap(short, long, default_value = "2018")]
+    /// Edition, panic if not `2015` or `2018`.
+    edition: String,
 }
 
 fn main() {
     let opts: Opts = Opts::parse();
 
     match opts.subcmd {
-        SubCommand::Run(t) => {
-            match crate::run::run(&t.file_name) {
-                Ok(url) => {
-                    if cfg!(target_os = "windows") {
-                        let status = Command::new("rundll32.exe")
-                            .args(&["url.dll,FileProtocolHandler", &url])
-                            .status()
-                            .expect("failed to execute process");
-                        assert!(status.success());
-                    } else if cfg!(target_os = "macos") || cfg!(target_os = "linux") {
-                        #[cfg(target_os = "macos")]
-                        let cmd = "open";
-                        #[cfg(target_os = "linux")]
-                        let cmd = "xdg-open";
+        SubCommand::Run(t) => match crate::run::run(&t) {
+            Ok(url) => {
+                if cfg!(target_os = "windows") {
+                    let status = Command::new("rundll32.exe")
+                        .args(&["url.dll,FileProtocolHandler", &url])
+                        .status()
+                        .expect("failed to open browser");
+                    assert!(status.success());
+                } else if cfg!(target_os = "macos") || cfg!(target_os = "linux") {
+                    #[cfg(target_os = "macos")]
+                    let cmd = "open";
+                    #[cfg(target_os = "linux")]
+                    let cmd = "xdg-open";
 
-                        #[cfg(any(target_os = "macos", target_os = "linux"))]
-                        {
-                            let status = Command::new(cmd)
-                                .arg(url)
-                                .status()
-                                .expect("failed to execute process");
-                            assert!(status.success());
-                        }
-                    } else {
-                        unimplemented!()
+                    #[cfg(any(target_os = "macos", target_os = "linux"))]
+                    {
+                        let status = Command::new(cmd)
+                            .arg(url)
+                            .status()
+                            .expect("failed to open browser");
+                        assert!(status.success());
                     }
-                }
-                Err(e) => {
-                    eprintln!("Failed to execute `run` command: {}", e);
-                    std::process::exit(1);
+                } else {
+                    unimplemented!()
                 }
             }
-        }
+            Err(e) => {
+                eprintln!("failed to execute `run` command: {}", e);
+                std::process::exit(1);
+            }
+        },
     }
 }
